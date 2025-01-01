49911419482cbbf79409dd93ca72c93f295b35f3
kimsinok0714 <javaenjoy0714@gmail.com>
2025-01-02 08:09:46 +0900

    Update and rename DirectoryInitializer to DirectoryInitializer.java

--- src/main/java/com/example/boardapp/runner/DirectoryInitializer.java
+++ src/main/java/com/example/boardapp/runner/DirectoryInitializer.java
@@ -9,20 +9,17 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
 
-// CommandLineRunner : Spring Boot에서 제공하는 인터페이스로, 
+// CommandLineRunner or ApplicationRunner : Spring Boot에서 제공하는 인터페이스로, 
 // 애플리케이션이 시작된 직후 (컨테이너 초기화된 후) 특정 로직을 실행하고자 할 때 사용됩니다. 이를 통해 초기화 작업이나 특정 비즈니스 로직을 애플리케이션 시작 시 수행할 수 있습니다.
 
 @Slf4j
 @Component
-public class DirectoryInitializer implements ApplicationRunner {
-    
+public class DirectoryInitializer implements ApplicationRunner {    
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         
         File uploadDir = new File("/tmp/uploads");
-
-        System.out.println("Call DirectoryInitializer");
 
         if (!uploadDir.exists()) {
 
